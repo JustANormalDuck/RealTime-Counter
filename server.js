0c4f7fcd64bf15bb2d3fe44ea0d118bff820de9f
JustANormalDuck <65052456+JustANormalDuck@users.noreply.github.com>
2021-01-13 14:21:15 -0500

    Added a room system

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 
-var clickCount = 0;
+var clickCount1 = 0;
+var clickCount2 = 0;
 
 const app=express();
 const server = http.createServer(app);
@@ -13,15 +14,24 @@ app.use(express.static(__dirname +'/FrontEnd'));
 io.on('connection', function(client) { 
 	console.log('Client connected...'); 
 	//when the server receives clicked message, do this
-    client.on('clicked', function(data) {
-          clickCount++;
-          if(clickCount==5)
-                clickCount=0;
+    client.on('clicked1', function(data) {
+          clickCount1++;
+          if(clickCount1==5)
+                clickCount1=0;
           //send a message to ALL connected clients
         
-          io.emit('buttonUpdate', clickCount);
+          io.emit('buttonUpdate1', clickCount1);
         
     });
+    client.on('clicked2', function(data) {
+      clickCount2++;
+      if(clickCount2==5)
+            clickCount2=0;
+      //send a message to ALL connected clients
+    
+      io.emit('buttonUpdate2', clickCount2);
+    
+});
 });
 
 const PORT = process.env.PORT || 3000;
